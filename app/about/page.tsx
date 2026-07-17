386addc19f1dce1b56cbc9dfe0612fc64d0840ad
pushkar189 <praj892003@gmail.com>
2026-07-17 22:20:54 +0530

    UI Updates: removed about cards, added floating call/whatsapp buttons

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,6 @@ import { Navbar } from '@/components/layout/Navbar'
 import { Footer } from '@/components/layout/Footer'
 import AboutHeroSection from '@/components/about/AboutHeroSection'
 import MissionVisionValuesSection from '@/components/about/MissionVisionValuesSection'
-import PremiumAboutContent from '@/components/about/PremiumAboutContent'
 import TimelineSection from '@/components/about/TimelineSection'
 import DoctorTeamSection from '@/components/about/DoctorTeamSection'
 import AboutStorySection from '@/components/about/AboutStorySection'
@@ -20,7 +19,6 @@ export default function AboutPage() {
       <main>
         <AboutHeroSection />
         <MissionVisionValuesSection />
-        <PremiumAboutContent />
         <TimelineSection />
         <DoctorTeamSection />
         <AboutStorySection />
